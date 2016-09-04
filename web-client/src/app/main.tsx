20c590791952386faccb92ea22537705b3a600a2
mbme <stribog.ua@gmail.com>
2016-09-04 13:13:45 +0300

    use mobx

--- web-client/src/app/main.tsx
+++ web-client/src/app/main.tsx
@@ -1,13 +1,54 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-const appEl = document.getElementById('app')
+import {action, observable, useStrict} from 'mobx'
+import {observer} from 'mobx-react'
+import DevTools from 'mobx-react-devtools'
 
-if (appEl) {
-  ReactDOM.render(
-    <h1>HELLO</h1>,
-    appEl
-  )
-} else {
-  console.error("can't find root el #app")
+// webpack variable: true if dev mode enabled
+declare const __DEV__: boolean
+
+// do not allow to modify state out of actions
+useStrict(true)
+
+class AppState {
+  @observable timer: number = 0
+
+  constructor() {
+    setInterval(this.updateTimer, 1000)
+  }
+
+  @action updateTimer = () => {
+    this.timer += 1
+  }
+
+  @action resetTimer = () => {
+    this.timer = 0
+  }
 }
+
+@observer
+class TimerView extends React.Component<{appState: AppState}, {}> {
+  render(): JSX.Element {
+    const devTools = __DEV__ ? <DevTools /> : undefined
+    return (
+      <div>
+        <button onClick={this.onReset}>
+          Seconds passed: {this.props.appState.timer}
+        </button>
+        {devTools}
+      </div>
+    )
+  }
+
+  onReset = () => {
+    this.props.appState.resetTimer()
+  }
+};
+
+const appState =  new AppState()
+
+ReactDOM.render(
+  <TimerView appState={appState} />,
+  document.getElementById('app')
+)
