2004a33f09aa9e0d2018442e5497e84f87401bdc
mbme <stribog.ua@gmail.com>
2016-10-06 21:11:33 +0300

    just warn if server version doesn't match client version

--- server/src/main.rs
+++ server/src/main.rs
@@ -25,7 +25,9 @@ use std::env;
 fn main() {
     let version = option_env!("GIT_COMMIT_HASH").unwrap_or("");
 
-    assert_eq!(version, server::UI_APP_VERSION, "Server version doesn't match client version");
+    if version != server::UI_APP_VERSION {
+        println!("WARN: server version {} doesn't match client version {}", server::UI_APP_VERSION, version);
+    }
 
     let path = env::var("V_CONFIG").unwrap_or("~/.config/v/config.json".into());
 
