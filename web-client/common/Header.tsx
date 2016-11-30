661f7c3c144339d500d393430a3a93233ccfa3f4
mbme <stribog.ua@gmail.com>
2016-11-30 19:23:48 +0200

    header: highlight current view link

--- web-client/common/Header.tsx
+++ web-client/common/Header.tsx
@@ -1,16 +1,25 @@
 import * as React from 'react'
+import * as cx from 'classnames'
 
 import {Inject} from 'web-client/injector'
 import Store from 'web-client/store'
+import { ViewTypes } from 'web-client/types'
 
 export class Header extends React.Component<{}, {}> {
   @Inject store: Store
 
+  renderLink(view: ViewTypes, text: string): JSX.Element {
+    const classes = cx('Header-link', { 'is-active': this.store.view === view })
+    return (
+      <div className={classes} onClick={() => this.store.setView('notes')}>{text}</div>
+    )
+  }
+
   render (): JSX.Element {
     return (
       <header className="Header">
-        <div className="Header-link" onClick={() => this.store.setView('notes')}>Notes</div>
-        <div className="Header-link" onClick={() => this.store.setView('todos')}>Todos</div>
+        {this.renderLink('notes', 'Notes')}
+        {this.renderLink('todos', 'Todos')}
         <div className="Header-right">{this.props.children}</div>
       </header>
     )
