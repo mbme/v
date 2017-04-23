f86d050ef04d47ee35348088cfd6126d3c69930e
mbme <stribog.ua@gmail.com>
2017-04-24 00:22:52 +0300

    WIP server

--- server/index.js
+++ server/index.js
@@ -4,15 +4,11 @@ const Route = require('route-parser')
 
 const createProcessor = require('./processor')
 
-const PORT = 8080
-
 const fileUploadRoute = new Route('/api/files/:record_id')
 const fileRoute = new Route('/api/files/:record_id/:file_name')
 const apiRoute = new Route('/api')
 
-const processor = await createProcessor()
-
-function parseRequestBody(req) {
+function parseRequestBody (req) {
   return new Promise((resolve, reject) => {
     const data = []
 
@@ -21,62 +17,97 @@ function parseRequestBody(req) {
   })
 }
 
-function handleRequest (req, res) {
-  const method = req.method.toLowerCase()
+async function parseForm (req) {
+  const form = new formidable.IncomingForm()
 
-  const fileUploadRouteResult = fileUploadRoute.match(req.url)
-  if (method === 'post' && fileUploadRouteResult) {
-    const form = new formidable.IncomingForm()
-
+  return new Promise((resolve, reject) => {
     form.parse(req, function (err, fields, files) {
-      res.writeHead(200, { 'content-type': 'text/plain' })
-      res.write('received upload:\n\n', files)
-      // res.end(util.inspect({fields: fields, files: files}))
-    })
-
-    const response = await processor.processAction({
-      name: 'CREATE_FILE',
-      data: {
-        record_id: fileUploadRouteResult.record_id,
-        name: '',
-        data: '',
+      if (err) {
+        reject(err)
+      } else {
+        resolve({ fields, files })
       }
     })
-    // write response
-
-    return
-  }
-
-  const fileRouteResult = fileRoute.match(req.url)
-  if (method === 'get' && fileRouteResult) {
-    const response = await processor.processAction({
-      name: 'READ_FILE',
-      data: {
-        record_id: fileRouteResult.record_id,
-        name: fileRouteResult.file_name,
-      }
-    })
-    // send response
-
-    return
-  }
-
-  if (method === 'post' && apiRoute.match(req.url)) {
-    // handle api request
-    const response = await processor.processAction(await parseRequestBody(req))
-
-    return
-  }
-
-  // write static files
+  })
 }
 
-const server = http.createServer(handleRequest)
+function writeResponse (res, response) {
+  res.writeHead(200, { 'content-type': 'application/json' })
+  res.write(JSON.stringify(response))
+}
 
-server.listen(PORT, function () {
-  console.log('Server listening on: http://localhost:%s', PORT)
-})
+async function createRequestHandler () {
+  const processor = await createProcessor()
 
-server.on('error', (error) => {
-  console.error(error)
-})
+  return async function handleRequest (req, res) {
+    const method = req.method.toLowerCase()
+
+    const fileUploadRouteResult = fileUploadRoute.match(req.url)
+    if (method === 'post' && fileUploadRouteResult) {
+      const { fields, files } = await parseForm(req)
+
+      // TODO save file
+
+      const response = await processor.processAction({
+        name: 'CREATE_FILE',
+        data: {
+          record_id: fileUploadRouteResult.record_id,
+          name: '',
+          data: ''
+        }
+      })
+
+      writeResponse(res, response)
+
+      return
+    }
+
+    const fileRouteResult = fileRoute.match(req.url)
+    if (method === 'get' && fileRouteResult) {
+      const response = await processor.processAction({
+        name: 'READ_FILE',
+        data: {
+          record_id: fileRouteResult.record_id,
+          name: fileRouteResult.file_name
+        }
+      })
+
+      if (response instanceof Buffer) {
+        res.writeHead(200, {}) // TODO write proper content type
+        res.write(response)
+      } else {
+        res.writeHead(400, { 'content-type': 'application/json' })
+        res.write(JSON.stringify(response))
+      }
+
+      return
+    }
+
+    if (method === 'post' && apiRoute.match(req.url)) {
+      // handle api request
+      const response = await processor.processAction(await parseRequestBody(req))
+
+      writeResponse(res, response)
+    }
+
+    // TODO write static files
+  }
+}
+
+async function startServer (port = 8080) {
+  const handler = await createRequestHandler()
+  const server = http.createServer(handler)
+
+  return new Promise((resolve) => {
+    server.on('error', (error) => {
+      console.error(error)
+    })
+
+    server.listen(port, function () {
+      console.log('Server listening on: http://localhost:%s', port)
+      resolve(server)
+    })
+  })
+}
+
+startServer()
