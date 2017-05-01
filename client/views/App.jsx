c619a425af079f16a7dcb51e04c74c3a23e670d7
mbme <stribog.ua@gmail.com>
2017-05-02 00:15:34 +0300

    babel, eslint

---
+++ client/views/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { observer } from 'mobx-react'
+
+function App () {
+  return (
+    <div>HELLO WORLD!</div>
+  )
+}
+
+export default observer(App)
--- webpack.config.js
+++ webpack.config.js
@@ -11,9 +11,30 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  resolve: {
+    extensions: [ '.js', '.jsx', '.json' ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          cacheDirectory: true,
+        },
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({ template: 'client/index.html' }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
   ],
+  devtool: 'cheap-module-eval-source-map',
 }
