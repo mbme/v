44fbd313d273fa51cc2a85091277f4ad739464fc
mbme <stribog.ua@gmail.com>
2017-11-08 00:15:59 +0200

    optimize build, target es6, don't use babel-polyfill

---
+++ webpack.config.babel.js
@@ -0,0 +1,71 @@
+import path from 'path'
+import webpack from 'webpack'
+import MinifyPlugin from 'babel-minify-webpack-plugin'
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    './client/index.jsx',
+  ],
+
+  output: {
+    publicPath: '/',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+
+  resolve: {
+    extensions: [ '.js', '.jsx', '.json' ],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            babelrc: false,
+            plugins: [
+              'react-hot-loader/babel',
+              'transform-object-rest-spread',
+              'transform-class-properties',
+              'syntax-jsx',
+              'transform-react-jsx',
+              [ 'module-resolver', { root: [ '.' ] } ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    isProduction && new webpack.optimize.ModuleConcatenationPlugin(),
+    isProduction && new MinifyPlugin(),
+
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
+  ].filter(plugin => !!plugin),
+
+  devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+
+  devServer: {
+    hot: true,
+    port: 8000,
+    historyApiFallback: true,
+    contentBase: [ path.join(__dirname, './static') ],
+    proxy: {
+      '/api': 'http://localhost:8080',
+    },
+  },
+}
