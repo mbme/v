6c49667963df478d0ce3ba9df967c3c374e7f515
mbme <stribog.ua@gmail.com>
2018-03-31 11:35:19 +0300

    add timeout before making ProgressLocker visible

--- client/chrome/ProgressLocker.jsx
+++ client/chrome/ProgressLocker.jsx
@@ -1,34 +1,60 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import s from 'client/styles';
 import { Icon, Backdrop } from 'client/components';
 
-const backdropStyles = s.cx({
-  cursor: 'progress',
-  animationName: s.animation({
-    '0%': { opacity: '0.7' },
-    '50%': { opacity: '1' },
-    '100%': { opacity: '0.7' },
+const styles = s.styles({
+  backdrop: visible => ({
+    cursor: 'progress',
+    alignItems: 'center',
+    opacity: 0,
+
+    extend: [
+      visible && {
+        animationName: s.animation({
+          '0%': { opacity: '0.7' },
+          '50%': { opacity: '1' },
+          '100%': { opacity: '0.7' },
+        }),
+        animationDuration: '3s',
+        animationIterationCount: 'infinite',
+      },
+    ],
   }),
-  animationDuration: '3s',
-  animationIterationCount: 'infinite',
-  alignItems: 'center',
+
+  spinner: {
+    width: '24px',
+    height: '24px',
+    animationName: s.animation({
+      from: { transform: 'rotate(0deg)' },
+      to: { transform: 'rotate(359deg)' },
+    }),
+    animationDuration: '1.5s',
+    animationIterationCount: 'infinite',
+  },
 });
 
-const spinnerStyles = s.cx({
-  width: '24px',
-  height: '24px',
-  animationName: s.animation({
-    from: { transform: 'rotate(0deg)' },
-    to: { transform: 'rotate(359deg)' },
-  }),
-  animationDuration: '1.5s',
-  animationIterationCount: 'infinite',
-});
+export default class ProgressLocker extends PureComponent {
+  state = {
+    visible: false,
+  };
 
-export default function ProgressLocker() {
-  return (
-    <Backdrop className={backdropStyles}>
-      <Icon type="loader" className={spinnerStyles} />
-    </Backdrop>
-  );
+  timer = null;
+
+  makeVisible = () => this.setState({ visible: true });
+
+  componentDidMount() {
+    this.timer = setTimeout(this.makeVisible, 1000);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timer);
+  }
+
+  render() {
+    return (
+      <Backdrop className={styles.backdrop(this.state.visible)}>
+        <Icon type="loader" className={styles.spinner} />
+      </Backdrop>
+    );
+  }
 }
