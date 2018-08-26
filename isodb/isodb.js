b60c3373ada074480365b366d038cf427a88690a
mbme <stribog.ua@gmail.com>
2018-08-26 20:38:48 +0300

    isodb wip

--- isodb/isodb.js
+++ isodb/isodb.js
@@ -2,21 +2,46 @@
 // client forms PATCH and fetches changes from master before pushing
 // (and retries until everything works)
 // so it doesn't matter how many time client updated record - there would be single revision
-{
-  _id: 'id',
-  _rev: '2-xfa2323zx2',
-  _conflicts: ['2-asdfasdfasd', '2-322sdfas'],
+{ // record
+  _id: 'zdfw234d2',
+  _rev: 2, // autoincrement
+  _refs: ['asdfsad'],
   _deleted: true,
-  _attachments: [],
+  name: 'test',
+  data: 'x1',
+}
+{ // attachment
+  _id: 'md5',
+  _rev: 2, // autoincrement
+  _deleted: true,
+  _attachment: true,
+  size: 10,
+}
+{ // patch
+  _id: 'md5',
+  _rev: 2, // autoincrement
+  _deleted: true,
+  size: 10,
 }
 
-export function createClient() {
-  // autosync
-  return {
-    sync() {},
+class Client {
+  _rev = 0;
+  _localRev = 0;
+
+  _fetchAll(rev) {}
+  _pushChanges(rev) {}
+  _extractPatch() {}
+
+  sync() {
+
   }
 }
 
-export function createMaster() { // the single source of trust
+class Server {
+  _rev = 0;
 
+  getAll(rev) {}
+
+  // patches: []patch
+  applyPatch(patches) {}
 }
